ab2c620c6059c420ff6b54e4333fbb3ac8c362ea
Alvaro Llamas <alvaro.llamas@gmail.com>
2024-10-07 11:42:02 +0200

    Segundo commit

--- dni.js
+++ dni.js
@@ -6,16 +6,22 @@
 
     // Declaración de variables
     let dni = prompt("Introduce un DNI");
-    let numeros = dni.slice(0,7);
-    let letra = dni.slice(8);
+    let numeros = dni.slice(0,8);
+    let letra = dni.slice(8).toUpperCase;
+    let suma;
+    let resto;
     let compruebaLetra;
     let letras = ["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"];
 
     document.write("Comprobamos si la letra del DNI es correcta");
 
-    compruebaLetra = numeros % 23;
+    // Sumamos y dividimos los números entre 23 y almacenamos el resto
+    resto = numeros % 23;
 
-    
-
-    document.write(numeros);
-    document.write(compruebaLetra);
+    // Condicional if que comprueba que la letra sea la misma que la que se encuentra
+    // en la posición del Array (la cual sacamos del resto)
+    if(letra === letras[resto]){
+        document.write("La letra del DNI es correcta");
+    } else {
+        document.write("La letra del DNI es incorrecta");
+    }
